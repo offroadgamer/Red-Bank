22273e964cf9705430612c96be446ff8cd57388d
olafkri05 <olafkri05@gmail.com>
2024-05-30 15:27:47 +0200

    Added withdraw and deposit functionality
    Added a close button so that you can close the transaction menu.
    I also added the functionality so that the transaction menu by default is hidden, but when you press "withdraw" or "deposit", it shows.

--- app.js
+++ app.js
@@ -1,5 +1,31 @@
 let balance = 0;
-document.getElementById("balance").innerHTML = balance;
+let status = 2;
+document.getElementById("transaction-box").style = 
+//Hvis status er 0, withdraw
+//Hvis status er 1, deposit
+
 function getMoney() {
     balance += 100;
+    document.getElementById("balance").innerHTML = "Your balance: $" + balance;
+}
+
+function deposit() {
+    status = 1;
+    document.getElementById("operation-text").innerHTML = "How much do you want to deposit?";
+}
+
+function withdraw() {
+    status = 0;
+    document.getElementById("operation-text").innerHTML = "How much do you want to withdraw?";
+}
+
+function transaction(amount) {
+    if (status == 0) {
+        balance -= amount;
+    } else if (status == 1){
+        balance += amount;
+    } else {
+        document.write("<h1>The transaction is invalid</h1>");
+    }
+    document.getElementById("balance").innerHTML = "Your balance: $" + balance;
 }
