3195bda4e7637bf5f8a13e965f70dfaa8f78cf24
olafkri05 <olafkri05@gmail.com>
2024-06-04 16:00:36 +0200

    Added functionality for logging in and signing up

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 let balance = 0;
-let status = 2;
+let transactionStatus = 2;
+let loginStatus = 2;
+usernames = ["gregor", "ola", "gjøl"];
+passwords = ["agjorg", "123", "jfsdj"];
 
-//Hvis status er 0, withdraw
-//Hvis status er 1, deposit
 
 function getMoney() {
     balance += 100;
@@ -10,24 +11,24 @@ function getMoney() {
 }
 
 function deposit() {
-    status = 1;
+    transactionStatus = 1;
     document.getElementById("operation-text").innerHTML = "How much do you want to deposit?";
     document.getElementById("transaction-box").style.visibility = "visible";
 }
 
 function withdraw() {
-    status = 0;
+    transactionStatus = 0;
     document.getElementById("operation-text").innerHTML = "How much do you want to withdraw?";
     document.getElementById("transaction-box").style.visibility = "visible";
 }
 
 function transaction(amount) {
-    if (status == 0) {
+    if (transactionStatus == 0) {
         balance -= amount;
-    } else if (status == 1){
+    } else if (transactionStatus == 1){
         balance += amount;
     } else {
-        document.write("<h1>The transaction is invalid</h1>");
+        document.write("<h1>The transaction is invalid</h1><br><a href='index.html'>Go back</a></error>");
     }
     document.getElementById("balance").innerHTML = "Your balance: $" + balance;
 }
@@ -35,3 +36,48 @@ function transaction(amount) {
 function closeTransaction() {
     document.getElementById("transaction-box").style.visibility = "hidden";
 }
+
+function navLogin() {
+    loginStatus = 1
+    document.getElementById("login-box").style.visibility = "visible";
+    document.getElementById("login-text").innerHTML = "Log in to red bank";
+    document.getElementById("signin").innerHTML = "Log in";
+}
+
+function navSignup() {
+    loginStatus = 0;
+    document.getElementById("login-box").style.visibility = "visible";
+    document.getElementById("login-text").innerHTML = "Sign up for red bank";
+    document.getElementById("signin").innerHTML = "Sign Up";
+}
+
+function login() {
+    username = document.getElementById("login-email-input").value;
+    password = document.getElementById("login-password-input").value;
+    if (loginStatus == 1) {
+        indexUsername = usernames.indexOf(username);
+        indexPassword = passwords.indexOf(password);
+        if(indexUsername == indexPassword) {
+            document.write("Match!")
+        } else {
+            document.write("Failed to log in...")
+        } 
+    } else if (loginStatus == 0) {
+        if(username == "" || username == " " || password == "" || password == " ") {
+            console.log("Invalid");
+            document.getElementById("error-fillout-login").style.visibility = "visible";
+        } else {
+            usernames.push(username);
+            passwords.push(password);
+            console.log("Valid")
+        }
+        document.getElementById("login-email-input").value = "";
+        document.getElementById("login-password-input").value = "";
+        console.log(usernames);
+        console.log(passwords);
+    } else {
+        document.write("<h1>Invalid login</h1><br><h2>Go back</h2>")
+    }
+    
+}
+
