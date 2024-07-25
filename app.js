fdfe299c0a1fdc9888787cfcc0dff726c23104f4
olafkri05 <olafkri05@gmail.com>
2024-07-25 16:02:54 +0200

    Login rework
    I have reworked the entire login system, and fixed loopholes and flaws with the old one.

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 let balance = 0;
 let transactionStatus = 2;
 let loginStatus = 2;
-usernames = ["gregor", "ola", "gjøl"];
-passwords = ["agjorg", "123", "jfsdj"];
+let = loggedIn = false;
+let accounts = [["ola", "123", 1000.00], ["gregor", "agurk", -700.00]]
 
 
 function getMoney() {
@@ -14,12 +14,14 @@ function deposit() {
     transactionStatus = 1;
     document.getElementById("operation-text").innerHTML = "How much do you want to deposit?";
     document.getElementById("transaction-box").style.visibility = "visible";
+    document.getElementById("message-box").style.visibility = "hidden";
 }
 
 function withdraw() {
     transactionStatus = 0;
     document.getElementById("operation-text").innerHTML = "How much do you want to withdraw?";
     document.getElementById("transaction-box").style.visibility = "visible";
+    document.getElementById("message-box").style.visibility = "hidden";
 }
 
 function transaction(amount) {
@@ -38,7 +40,7 @@ function closeTransaction() {
 }
 
 function navLogin() {
-    loginStatus = 1
+    loginStatus = 1;
     document.getElementById("login-box").style.visibility = "visible";
     document.getElementById("login-text").innerHTML = "Log in to red bank";
     document.getElementById("signin").innerHTML = "Log in";
@@ -51,42 +53,61 @@ function navSignup() {
     document.getElementById("signin").innerHTML = "Sign Up";
 }
 
+function closeMessage() {
+    document.getElementById("message-box").style.visibility = "hidden";
+}
+
 function login() {
-    username = document.getElementById("login-email-input").value;
-    password = document.getElementById("login-password-input").value;
+    document.getElementById("message-box").style.visibility = "hidden";
+    document.getElementById("transaction-box").style.visibility = "hidden";
+    let username_f = document.getElementById("login-email-input").value;
+    let password_f = document.getElementById("login-password-input").value;
+    let username = username_f.replace(" ", "");
+    let password = password_f.replace(" ", "");
+    console.log(username, password);
     if (loginStatus == 1) {
-        indexUsername = usernames.indexOf(username);
-        indexPassword = passwords.indexOf(password);
         document.getElementById("login-email-input").value = "";
         document.getElementById("login-password-input").value = "";
-        if(indexUsername == indexPassword) {
+        let usrn_index = 0;
+        let pswd_index = 0;
+        let is_valid = false;
+        for (array_number = 0; array_number < accounts.length; array_number++) {
+            if (accounts[array_number][0] == username && accounts[array_number][1] == password) {
+                usrn_index = array_number;
+                pswd_index = array_number;
+                is_valid = true;
+            }
+        }
+        if(is_valid && usrn_index == pswd_index) {
+            document.getElementById("message-box").style.visibility = "visible";
             document.getElementById("login-box").style.visibility = "hidden";
-            document.getElementById("message").innerHTML = "Logged in successfully";
+            document.getElementById("message").innerHTML = "Logged in successfully!";
             document.getElementById("message").style.color = "#66cc66";
             document.getElementById("message-box").style.visibility = "visible";
+            console.log("SUCSESS!");
+            loggedIn = true;
+            balance = accounts[usrn_index][2];
+            document.getElementById("balance").innerHTML = "Your balance: $" + balance;
         } else {
+            document.getElementById("message-box").style.visibility = "visible";
             document.getElementById("login-box").style.visibility = "hidden";
             document.getElementById("message").innerHTML = "Login Failed";
             document.getElementById("message").style.color = "#e13423";
             document.getElementById("message-box").style.visibility = "visible";
-        } 
+            console.log("FAILED!");
+        }
     } else if (loginStatus == 0) {
         if(username == "" || username == " " || password == "" || password == " ") {
             console.log("Invalid");
             document.getElementById("error-fillout-login").style.visibility = "visible";
         } else {
-            usernames.push(username);
-            passwords.push(password);
+            accounts.push([username, password, 0.00]);
             console.log("Valid");
             document.getElementById("login-box").style.visibility = "hidden";
-            document.getElementById("message").innerHTML = "Logged in successfully";
+            document.getElementById("message").innerHTML = "Signed Up successfully!";
             document.getElementById("message").style.color = "#66cc66";
             document.getElementById("message-box").style.visibility = "visible";
         }
-        document.getElementById("login-email-input").value = "";
-        document.getElementById("login-password-input").value = "";
-        console.log(usernames);
-        console.log(passwords);
     } else {
         document.write("<h1>Invalid login</h1><br><h2>Go back</h2>")
     }
